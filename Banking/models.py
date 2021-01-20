8bfa65647b9b1a861351c338d05bc5039fae680f
saimohancharugundla <saimohancharugundla@gmail.com>
2021-01-20 21:31:13 +0530

    Final

--- Banking/models.py
+++ Banking/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 class Customers(models.Model):
     cust_name = models.CharField(max_length=15)
-    amount = models.CharField(max_length=10,default=0)
+    amount = models.IntegerField(default=0)
     transactions = models.DateTimeField(auto_now_add=True,blank=False,null=True)
     
     
